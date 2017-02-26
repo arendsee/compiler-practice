a7f02c30f5e500ac28dfe85dc63dd162965aa944
Zebulun Arendsee <arendsee@iastate.edu>
2017-02-25 20:01:33 -0600

    Add a rough type parser
    There is an ambiguity that I will need to hash out, but I have no time.
    Will be back shortly.

--- lrparsing/example.py
+++ lrparsing/example.py
@@ -11,16 +11,36 @@ from lrparsing import Keyword, List, Prio, Ref, THIS, Token, Tokens
 class ExprParser(lrparsing.Grammar):
 
     class T(lrparsing.TokenRegistry):
-        integer = Token(re="[0-9]+")
         ident = Token(re="[A-Za-z_][A-Za-z_0-9]*")
 
-    expr = Ref("expr")
-    atom = T.ident | T.integer | '(' + expr + ')'
-    expr = Prio(
-        atom,
-        THIS << '+' << THIS
-    )
-    START = expr
+    l_type = Ref("l_type")
+    l_atom = T.ident
+    l_tuple = '(' + List(l_type, ',') + ')'
+    l_func = '(' + List(l_type, '->') + ')'
+    l_list = '[' + l_type + ']'
 
-parse_tree = ExprParser.parse("b + 1 + 42 + (3 + foo)")
+    l_type = l_atom | l_tuple | l_func | l_list
+    START = l_type 
+
+parse_tree = ExprParser.parse("(a,b,c)")
 print(ExprParser.repr_parse_tree(parse_tree))
+
+#  import lrparsing
+#  from lrparsing import Keyword, List, Prio, Ref, THIS, Token, Tokens
+#
+#  class ExprParser(lrparsing.Grammar):
+#
+#      class T(lrparsing.TokenRegistry):
+#          integer = Token(re="[0-9]+")
+#          ident = Token(re="[A-Za-z_][A-Za-z_0-9]*")
+#
+#      expr = Ref("expr")
+#      atom = T.ident | T.integer | '(' + expr + ')'
+#      expr = Prio(
+#          atom,
+#          THIS << '+' << THIS
+#      )
+#      START = expr
+#
+#  parse_tree = ExprParser.parse("b + 1 + 42 + (3 + foo)")
+#  print(ExprParser.repr_parse_tree(parse_tree))
