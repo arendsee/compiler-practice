8a26c400539e4763d3c33520a699ec05906920d8
Zebulun Arendsee <arendsee@iastate.edu>
2017-02-27 00:15:29 -0600

    Simply arpeggio type parser

--- arpeggio/types/haskell_types.py
+++ arpeggio/types/haskell_types.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from __future__ import unicode_literals
-
 from arpeggio import *
 from arpeggio import RegExMatch as _
 
@@ -16,47 +14,29 @@ def typeType():  return typeExpr, EOF
 class typeTypeVisitor(PTNodeVisitor):
 
     def visit_typeIdent(self, node, children):
-        if self.debug:
-            print(" - Entering typeIdent")
-            print(node.value)
         return(node.value)
 
     def visit_typeTuple(self, node, children):
-        if self.debug:
-            print(" - Entering typeTuple")
-            print(node.value)
         return("tuple(%s)" % ','.join(children))
 
     def visit_typeArray(self, node, children):
-        if self.debug:
-            print(" - Entering typeArray")
-            print(node.value)
         return("array(%s)" % children[0])
 
     def visit_typeFunc(self, node, children):
-        if self.debug:
-            print(" - Entering typeFunc")
-            print(node.value)
         return("function(%s)" % ','.join(children))
 
     def visit_typeExpr(self, node, children):
-        if self.debug:
-            print(" - Entering typeExpr")
-            print(node.value)
         return(children[0])
 
     def visit_typeType(self, node, children):
-        if self.debug:
-            print(" - Entering typeType")
-            print(node.value)
         return(children[0])
 
-def main(debug=False):
-    parser = ParserPython(typeType, debug=debug)
+def main():
+    parser = ParserPython(typeType)
 
     parse_tree = parser.parse("([(a,b,c)],(x->z))")
 
-    result = visit_parse_tree(parse_tree, typeTypeVisitor(debug=False))
+    result = visit_parse_tree(parse_tree, typeTypeVisitor())
 
     print(result)
 
