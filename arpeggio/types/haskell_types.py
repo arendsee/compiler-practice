127e1855ca74f7c77ff763ede91dc1edefe61ae1
Zebulun Arendsee <arendsee@iastate.edu>
2017-02-27 00:00:42 -0600

    Parse type string to nested functions
    Parse the type `([(a,b,c)],(x->z))` into
    
    ```
    tuple(array(tuple(a,b,c)),function(x,z))
    ```
    
    This is actually pretty clean and easy. I prefer it to lrparsing (at the
    moment). PEG has its perks. Not having to deal with ambiguity is one of
    them. But I don't know the algorithm, so I hesitate to judge.

--- arpeggio/types/haskell_types.py
+++ arpeggio/types/haskell_types.py
@@ -2,12 +2,11 @@
 
 from __future__ import unicode_literals
 
-import os
 from arpeggio import *
 from arpeggio import RegExMatch as _
 
 def typeIdent(): return _('[A-Za-z0-9_]+')
-def typeTuple(): return '(', typeExpr, ZeroOrMore(',', typeExpr), ')'
+def typeTuple(): return '(', typeExpr, OneOrMore(',', typeExpr), ')'
 def typeArray(): return '[', typeExpr, ']'
 def typeFunc():  return '(', typeExpr, OneOrMore('->', typeExpr), ')'
 def typeExpr():  return [typeIdent, typeTuple, typeArray, typeFunc]
@@ -19,33 +18,47 @@ class typeTypeVisitor(PTNodeVisitor):
     def visit_typeIdent(self, node, children):
         if self.debug:
             print(" - Entering typeIdent")
+            print(node.value)
+        return(node.value)
 
     def visit_typeTuple(self, node, children):
         if self.debug:
             print(" - Entering typeTuple")
+            print(node.value)
+        return("tuple(%s)" % ','.join(children))
 
     def visit_typeArray(self, node, children):
         if self.debug:
             print(" - Entering typeArray")
+            print(node.value)
+        return("array(%s)" % children[0])
 
     def visit_typeFunc(self, node, children):
         if self.debug:
             print(" - Entering typeFunc")
+            print(node.value)
+        return("function(%s)" % ','.join(children))
 
     def visit_typeExpr(self, node, children):
         if self.debug:
             print(" - Entering typeExpr")
+            print(node.value)
+        return(children[0])
 
     def visit_typeType(self, node, children):
         if self.debug:
             print(" - Entering typeType")
+            print(node.value)
+        return(children[0])
 
 def main(debug=False):
     parser = ParserPython(typeType, debug=debug)
 
-    parse_tree = parser.parse("(a,b,c)")
+    parse_tree = parser.parse("([(a,b,c)],(x->z))")
 
-    result = visit_parse_tree(parse_tree, typeTypeVisitor(debug=True))
+    result = visit_parse_tree(parse_tree, typeTypeVisitor(debug=False))
+
+    print(result)
 
 if __name__ == "__main__":
     main()
